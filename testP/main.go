d1955917f5ee5a804a1a1ae83ecefe88be2f9628
doris <64065933@qq.com>
2023-05-04 10:15:22 +0800

    [chore]增加配置信息

--- testP/main.go
+++ testP/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"mywork/learning/testP/random"
+	"time"
+)
+
 // 测试函数
-func main() {}
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	fmt.Println("测试数据", random.RandStr(10))
+}
